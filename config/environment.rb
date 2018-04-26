22bc98a5cf6626646c6feabe8db5add8fbdfa3ab
Ichiki Ryo <tohodaiichiki@gmail.com>
2018-04-26 21:51:18 +0900

    fix mailer config for heroku

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+ActionMailer::Base.smtp_settings = {
+  :address => 'smtp.sendgrind.net',
+  :port =. '587',
+  :authentication => :plain,
+  :user_name => ENV['MAILER_USER'],
+  :password => ENV['MAILER_PASS'],
+  :domain => 'heroku.com',
+  :enable_starttls_auto => true
+}
