4220d72b346b3889180992a1d9feb73fed590c24
Ichiki Ryo <tohodaiichiki@gmail.com>
2018-04-26 23:17:57 +0900

    Add config production sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,19 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Setup the mailer config
+  # Use SendGrid - Add-ons - Heroku
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'cafeshaker.herokuapp.com' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    user_name: ENV['SENDGRID_USER'],
+    password: ENV['SENDGRID_PASS'],
+    domain: 'herokuapp.com',
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 end
