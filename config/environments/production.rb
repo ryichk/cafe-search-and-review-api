1a1dacf62630523bbfc1e24ea435d164851d5910
Ichiki Ryo <tohodaiichiki@gmail.com>
2018-04-29 18:53:43 +0900

    fix production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,6 +94,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   host = 'cafeshaker.herokuapp.com'
+  config.action_mailer.default_url_options = { :host => host }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => 'smtp.gmail.com',
