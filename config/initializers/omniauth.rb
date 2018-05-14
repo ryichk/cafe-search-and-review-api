779a1dee3351894817486e3a1f62f95811fb695b
Ichiki Ryo <tohodaiichiki@gmail.com>
2018-05-15 02:37:04 +0900

    add aws-sdk-s3 for Gemfile

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,11 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, Rails.application.secrets.twitter_api_key,
-                     Rails.application.secrets.twitter_api_secret
+  # facebook scopeを指定し、使用する権限を明記する必要があります
+  provider :facebook, Rails.application.secrets.facebook_id, Rails.application.secrets.facebook_secret_key, scope: 'email,status_update,publish_actions'
+  # Twitter
+  provider :twitter, Rails.application.secrets.twitter_api_key,Rails.application.secrets.twitter_api_secret
+end
+
+#キャンセル
+OmniAuth.config.on_failure = Proc.new do |env|
+  OmniAuth::FailureEndpoint.new(env).redirect_to_failure
 end
