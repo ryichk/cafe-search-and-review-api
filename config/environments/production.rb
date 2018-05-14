24f2c38f4ec131c8b02ffb9fe251ecbdcad9612c
Ichiki Ryo <tohodaiichiki@gmail.com>
2018-05-15 08:05:20 +0900

    fix mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,7 +99,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
-    :address => 'smtp.gmail.net',
+    :address => 'smtp.gmail.com',
     :port => 587,
     :user_name => ENV['GMAIL_USRE'],
     :password => ENV['GMAIL_PASS'],
