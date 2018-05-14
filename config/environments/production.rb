cadf6f6be69aa2ffcba3e6727616b2833b76f908
Ichiki Ryo <tohodaiichiki@gmail.com>
2018-05-15 07:27:50 +0900

    fix production.rb for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,8 +103,8 @@ Rails.application.configure do
     :port => 587,
     :user_name => ENV['GMAIL_USRE'],
     :password => ENV['GMAIL_PASS'],
-    :domain => 'cafeshares.com'
-    :authentication => 'plain',
+    :domain => 'cafeshares.com',
+    :authentication => 'login',
     :enable_starttls_auto => true
   }
 
