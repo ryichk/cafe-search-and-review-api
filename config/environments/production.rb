ad694129c4f6d199b77255b399f7ac42a5f7a827
Ichiki Ryo <tohodaiichiki@gmail.com>
2018-05-21 01:10:35 +0900

    fix mailer config delivery_errors for true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'https://cafeshares.com' }
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
     :address => 'smtp.sendgrid.net',
     :port => "465",
