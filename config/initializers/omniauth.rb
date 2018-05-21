752ed6b22a1b998ff279b508b9cfb64af34968c1
Ichiki Ryo <tohodaiichiki@gmail.com>
2018-05-22 00:33:13 +0900

    fix omniauth.rb for instagram

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,6 +3,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, Rails.application.secrets.facebook_id, Rails.application.secrets.facebook_secret_key, scope: 'email,status_update,publish_actions'
   # Twitter
   provider :twitter, Rails.application.secrets.twitter_api_key,Rails.application.secrets.twitter_api_secret
+  # Instagram
+  provider :instagram, Rails.application.secrets.instagram_client_id, Rails.application.secrets.instagram_client_secret
 end
 
 #キャンセル
