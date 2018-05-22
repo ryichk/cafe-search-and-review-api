9dde17c1162e4e99bb328218db851fc513cb9110
Ichiki Ryo <tohodaiichiki@gmail.com>
2018-05-22 19:27:17 +0900

    fix api config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   # facebook scopeを指定し、使用する権限を明記する必要があります
   provider :facebook, Rails.application.secrets.facebook_id, Rails.application.secrets.facebook_secret_key, scope: 'email,status_update,publish_actions'
   # Twitter
-  provider :twitter, Rails.application.secrets.twitter_api_key,Rails.application.secrets.twitter_api_secret
+  provider :twitter, Rails.application.secrets.twitter_api_key, Rails.application.secrets.twitter_api_secret
   # Instagram
   provider :instagram, Rails.application.secrets.instagram_client_id, Rails.application.secrets.instagram_client_secret
 end
