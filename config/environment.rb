3e6b9a90ef17db414e01c8cce6566436777d1580
Ichiki Ryo <tohodaiichiki@gmail.com>
2018-05-24 08:48:49 +0900

    fix sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_API_USER'],
   :password => ENV['SENDGRID_API_PASS'],
