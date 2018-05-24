54ddb9a2e540bca93e3b644ea94de9ada3e26b44
Ichiki Ryo <tohodaiichiki@gmail.com>
2018-05-24 22:54:24 +0900

    test sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ require_relative 'application'
 Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_API_USER'],
-  :password => ENV['SENDGRID_API_PASS'],
+  :password => ENV['SENDGRID_API_KEY'],
   :domain => 'cafeshares.com',
   :address => 'smtp.sendgrid.net',
   :port => 465,
