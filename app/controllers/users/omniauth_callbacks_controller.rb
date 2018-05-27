add478bf4eadc00c583d802c93aa23d5410dfab2
Ichiki Ryo <tohodaiichiki@gmail.com>
2018-05-28 08:26:13 +0900

    test omniauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -41,14 +41,22 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def callback_from(provider)
       provider = provider.to_s
 
-      @user = User.find_for_oauth(request.env['omniauth.auth'], current_user)
+      @user = User.find_or_create_by(user_params)
 
       if @user.persisted? #保存済みかどうかを確認
-        flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
         sign_in_and_redirect @user, :event => :authentication
+        set_flash_message(:notice, :success, :kind => provider) if is_navigational_format?
       else
         session["devise.#{provider}_data"] = request.env['omniauth.auth']
         redirect_to new_user_registration_url
       end
     end
+
+    def failure
+      redirect_to root_path
+    end
+      private
+      def user_params
+        request.env["omniauth.auth"].slice(:provider, :uid).to_h
+      end
 end
