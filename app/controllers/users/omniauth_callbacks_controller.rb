c6fd86f0a24a16842763dd06805d3cc6a58fe550
Ichiki Ryo <tohodaiichiki@gmail.com>
2018-06-01 17:05:57 +0900

    test facebook auth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -40,7 +40,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     private
       def callback_from(provider)
         provider = provider.to_s
-        if provider == facebook
+        if provider == "facebook"
           @user = User.find_for_facebook_oauth(request.env['omniauth.auth'])
           if @user.persisted? #保存済みかどうかを確認
             flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
@@ -49,7 +49,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
             session["devise.#{provider}_data"] = request.env['omniauth.auth']
             redirect_to new_user_registration_url
           end
-        elsif provider == twitter
+        elsif provider == "twitter"
           @user = User.find_for_twitter_oauth(request.env['omniauth.auth'])
           if @user.persisted? #保存済みかどうかを確認
             flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
@@ -58,7 +58,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
             session["devise.#{provider}_data"] = request.env['omniauth.auth']
             redirect_to new_user_registration_url
           end
-        elsif provider == instagram
+        elsif provider == "instagram"
           @user = User.find_for_instagram_oauth(request.env['omniauth.auth'])
           if @user.persisted? #保存済みかどうかを確認
             flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
