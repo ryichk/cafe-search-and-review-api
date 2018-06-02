6201011de5cf82f064a365ec2288c537bdb28f67
Ichiki Ryo <tohodaiichiki@gmail.com>
2018-06-03 01:41:38 +0900

    fix user show place's name of reviews

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @username = current_user.username
-    @reviews = current_user.reviews
+    @reviews = current_user.reviews.includes(:place).order("created_at DESC")
   end
 
 end
