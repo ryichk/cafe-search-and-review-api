296e752df2dde853772bda4d1cb2a47db9f7462c
Ichiki Ryo <tohodaiichiki@gmail.com>
2018-06-02 23:19:26 +0900

    fix carrierwave config

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
   has_many :like_places, dependent: :destroy
   # belongs_to :place
 
-  mount_uploader :avatar_url, Avatar_urlUploader
+  mount_uploader :avatar_url, AvatarUrlUploader
   before_destroy :clean_s3
 
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
