c791c6b4d6fe43236a25f2fa2cb63722e3e55daf
Ichiki Ryo <tohodaiichiki@gmail.com>
2018-06-03 01:46:13 +0900

    fix user_controller username

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    @username = current_user.username
     @reviews = current_user.reviews.includes(:place).order("created_at DESC")
   end
 
