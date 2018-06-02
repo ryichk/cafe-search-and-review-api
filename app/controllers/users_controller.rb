43d2dc377f2413dec1e9d478589511fe56949469
Ichiki Ryo <tohodaiichiki@gmail.com>
2018-06-03 02:30:31 +0900

    fix user_controller show user_id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,8 @@ class UsersController < ApplicationController
   end
 
   def show
-    id = current_user.id
-    @reviews = Review.includes(:place).where(user_id: id).references(:review)
+    id_user = current_user.id
+    @reviews = Review.includes(:place).references(:review).where(user_id: id_user)
   end
 
 end
