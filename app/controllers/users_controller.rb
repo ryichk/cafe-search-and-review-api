1875cd56d54412394f68d922189cb526c232f180
Ichiki Ryo <tohodaiichiki@gmail.com>
2018-06-06 21:37:04 +0900

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: :show
   def index
-    @users = User.page(params[:page]).per(3)
+    @users = User.page(params[:page]).per(30)
   end
 
   def show
