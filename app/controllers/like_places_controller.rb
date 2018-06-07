49c67d12066192165abeb6928a5b1b83f42817bf
Ichiki Ryo <tohodaiichiki@gmail.com>
2018-06-08 08:10:35 +0900

    fix like and like place redirect and Show place reviews style

--- app/controllers/like_places_controller.rb
+++ app/controllers/like_places_controller.rb
@@ -7,33 +7,25 @@ class LikePlacesController < ApplicationController
 
   def create
     @place = Place.find(params[:place_id])
-    if user_signed_in?
-      unless @place.good?(current_user)
-        @place.good(current_user)
-        @place.reload
-        respond_to do |format|
-          format.html { redirect_to request.referrer || root_url }
-          format.js
-        end
+    unless @place.good?(current_user)
+      @place.good(current_user)
+      @place.reload
+      respond_to do |format|
+        format.html { redirect_to request.referrer || root_url }
+        format.js
       end
-    else
-      redirect_to user_session_path
     end
   end
 
   def destroy
     @place = LikePlace.find(params[:id]).place
-    if user_signed_in?
-      if @place.good?(current_user)
-        @place.ungood(current_user)
-        @place.reload
-        respond_to do |format|
-          format.html { redirect_to request.referrer || root_url }
-          format.js
-        end
+    if @place.good?(current_user)
+      @place.ungood(current_user)
+      @place.reload
+      respond_to do |format|
+        format.html { redirect_to request.referrer || root_url }
+        format.js
       end
-    else
-      redirect_to user_session_path
     end
   end
 end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,33 +3,25 @@ class LikesController < ApplicationController
 
   def create
     @review = Review.find(params[:review_id])
-    if user_signed_in?
-      unless @review.like?(current_user)
-        @review.like(current_user)
-        @review.reload
-        respond_to do |format|
-          format.html { redirect_to request.referrer || root_url }
-          format.js
-        end
+    unless @review.like?(current_user)
+      @review.like(current_user)
+      @review.reload
+      respond_to do |format|
+        format.html { redirect_to request.referrer || root_url }
+        format.js
       end
-    else
-      redirect_to user_session_path
     end
   end
 
   def destroy
     @review = Like.find(params[:id]).review
-    if user_signed_in?
-      if @review.like?(current_user)
-        @review.dislike(current_user)
-        @review.reload
-        respond_to do |format|
-          format.html { redirect_to request.referrer || root_url }
-          format.js
-        end
+    if @review.like?(current_user)
+      @review.dislike(current_user)
+      @review.reload
+      respond_to do |format|
+        format.html { redirect_to request.referrer || root_url }
+        format.js
       end
-    else
-      redirect_to user_session_path
     end
   end
 end
