63bd4b5ea9fd88d50695b40ecc706aa28d0ddfc8
Ichiki Ryo <tohodaiichiki@gmail.com>
2018-06-09 01:26:29 +0900

    fix user index order created_at DESC

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: :show
   def index
-    @users = User.page(params[:page]).per(30)
+    @users = User.page(params[:page]).order("created_at DESC").per(30)
   end
 
   def search
