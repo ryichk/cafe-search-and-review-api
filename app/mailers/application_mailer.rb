2e1176bd56a02892e73b1591f4ad8470d92206da
Ichiki Ryo <tohodaiichiki@gmail.com>
2018-06-09 09:01:52 +0900

    send mail test

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,3 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: '管理人 <from@cafeshares.com>'
   layout 'mailer'
 end
--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,14 +1,14 @@
 class InquiryMailer < ActionMailer::Base
 
   # 送信元アドレス
-  default from: "sendgrid.net"
+  default from: "cafeshares.com"
+
   # 送信先アドレス
-  default to: "ryichikio@gmail.com"
 
   def sendmail_confirm(inquiry)
+    mail to: "tohodaiichiki@gmail.com",
     # メール件名
-    mail_subject = "Cafe Sharesの問い合わせフォールから問い合わせがありました。"
+    subject: "Cafe Sharesの問い合わせフォームから問い合わせがありました。"
     @inquiry = inquiry
-    
   end
 end
