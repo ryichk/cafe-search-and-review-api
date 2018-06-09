1a968cd470f4427647585684aad952824315f212
Ichiki Ryo <tohodaiichiki@gmail.com>
2018-06-09 09:44:29 +0900

    fix turbolinks false

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -6,9 +6,9 @@ class InquiryMailer < ActionMailer::Base
   default to: "ryichikio@gmail.com"
 
   def sendmail_confirm(inquiry)
-    # メール件名
-    mail_subject = "Cafe Sharesの問い合わせフォームから問い合わせがありました。"
     @inquiry = inquiry
+    # メール件名
+    mail_subject = 'Cafe Sharesの問い合わせフォームから問い合わせがありました。'
     mail(:subject => mail_subject)
   end
 end
