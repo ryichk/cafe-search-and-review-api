d06359d3b2c9f00efaf7c3880c6e0cdbcbc6e750
Ichiki Ryo <tohodaiichiki@gmail.com>
2018-06-09 21:58:25 +0900

    fix user review order DESC

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.includes(reviews: :place).find(params[:id]).order("created_at DESC")
+    @user = User.includes(reviews: :place).find(params[:id]).order("created_at DESC").references(:reviews)
   end
 
 end
