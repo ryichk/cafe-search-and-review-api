56fe3173485a04679ad2760e39425b0b500cacd4
Ichiki Ryo <tohodaiichiki@gmail.com>
2018-06-09 23:58:57 +0900

    Add en.yml and de.yml local

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :detect_locate
 
   # 例外処理
   rescue_from ActiveRecord::RecordNotFound, with: :render_404
@@ -24,6 +25,10 @@ class ApplicationController < ActionController::Base
       def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :avatar_url, :agreement])
         devise_parameter_sanitizer.permit(:account_update, keys: [:username, :avatar_url])
+      end
+
+      def  detect_locate
+        I18n.locale = request.headers['Accept-Language'].scan(/\A[a-z]{2}/).first
       end
 
 
