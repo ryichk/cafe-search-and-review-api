44a649353eebf0a6f6315cf4873086f54042678e
Ichiki Ryo <tohodaiichiki@gmail.com>
2018-06-09 22:34:12 +0900

    fix mailer

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,14 +1,13 @@
 class InquiryMailer < ApplicationMailer
 
-  # # 送信元アドレス
-  # default from: "cafeshares.com"
+  # 送信元アドレス
+  default from: "cafeshares.com"
   # 送信先アドレス
   default to: "ryichikio@gmail.com"
 
   def sendmail_confirm(inquiry)
     @inquiry = inquiry
     # メール件名
-    mail_subject = 'Cafe Sharesの問い合わせフォームから問い合わせがありました。'
-    mail to: "ryichikio@gmail.com", subject: mail_subject
+    mail(:subject => 'Cafe Sharesの問い合わせフォームから問い合わせがありました。')
   end
 end
