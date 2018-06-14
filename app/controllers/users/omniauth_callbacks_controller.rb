ac3a0ed763286e5f285a7923a49b52a20f0c077b
Ichiki Ryo <tohodaiichiki@gmail.com>
2018-06-15 00:15:24 +0900

    test omiauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -29,7 +29,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def twitter
-    callback_for(:twitter)
+    callback_from :twitter
   end
 
   def instagram
@@ -40,25 +40,35 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     private
       def callback_from(provider)
         provider = provider.to_s
-        if provider == "facebook"
-          @user = User.find_for_facebook_oauth(request.env['omniauth.auth'])
-          if @user.persisted? #保存済みかどうかを確認
-            flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
-            sign_in_and_redirect @user, event: :authentication
-          else
-            session["devise.#{provider}_data"] = request.env['omniauth.auth']
-            redirect_to new_user_registration_url
-          end
-        elsif provider == "instagram"
-          @user = User.find_for_instagram_oauth(request.env['omniauth.auth'])
-          if @user.persisted? #保存済みかどうかを確認
-            flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
-            sign_in_and_redirect @user, event: :authentication
-          else
-            session["devise.#{provider}_data"] = request.env['omniauth.auth']
-            redirect_to new_user_registration_url
-          end
+        @user = User.find_for_oauth(request.env['omniauth.auth'])
+        if @user.persisted?
+          flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
+          sign_in_and_redirect @user, event: :authentication
+        else
+          session["devise.#{provider}_data"] = request.env['omniauth.auth']
+          redirect_to new_user_registration_url
         end
+      end
+    end
+        # if provider == "facebook"
+        #   @user = User.find_for_facebook_oauth(request.env['omniauth.auth'])
+        #   if @user.persisted? #保存済みかどうかを確認
+        #     flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
+        #     sign_in_and_redirect @user, event: :authentication
+        #   else
+        #     session["devise.#{provider}_data"] = request.env['omniauth.auth']
+        #     redirect_to new_user_registration_url
+        #   end
+        # elsif provider == "instagram"
+        #   @user = User.find_for_instagram_oauth(request.env['omniauth.auth'])
+        #   if @user.persisted? #保存済みかどうかを確認
+        #     flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
+        #     sign_in_and_redirect @user, event: :authentication
+        #   else
+        #     session["devise.#{provider}_data"] = request.env['omniauth.auth']
+        #     redirect_to new_user_registration_url
+        #   end
+        # end
     end
 
     def callback_for(provider)
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable, :timeoutable, :omniauthable, omniauth_providers: %i[facebook twitter]
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable, :timeoutable, :omniauthable
   validates :username, length: { in: 2..10, too_long: "最大%{count}文字までです。", too_short: "最低限%{count}文字必要です" }, presence: true
   validates :agreement, presence: true
   validates_acceptance_of :agreement, allow_nil: false, message: "＊ユーザー登録には利用規約への同意が必要です。", on: :create
@@ -18,40 +18,54 @@ class User < ApplicationRecord
   end
   before_destroy :clean_s3
 
-  def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
-    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+  def self.find_for_oauth(auth)
+    user = User.where(uid: auth.uid, provider: auth.provider).first
+
     unless user
-      user = User.create(username: auth.extra.raw_info.name,
-                         provider: auth.provider,
-                         uid: auth.uid,
-                         email: auth.info.email,
-                         password: Devise.friendly_token[0,20],
-                         agreement: 1
-                         )
+      user = User.create(
+        uid: auth.uid,
+        provider: auth.provider,
+        email: User.dummy_email(auth),
+        password: Devise.friendly_token[0, 20]
+        )
     end
     user
   end
 
-  def self.find_for_instagram_for_oauth(auth, signed_in_resource=nil)
-    user = User.where(:provider => auth.provider, :uid => auth.uid).first
-    unless user
-      user = User.create(username: auth.info.name,
-                         provider: auth.provider,
-                         uid: auth.uid,
-                         email: User.info.email,
-                         password: Devise.friendly_token[0,20],
-                         agreement: 1
-                         )
-    end
-    user
-  end
+  # def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+  #   user = User.where(:provider => auth.provider, :uid => auth.uid).first
+  #   unless user
+  #     user = User.create(username: auth.extra.raw_info.name,
+  #                        provider: auth.provider,
+  #                        uid: auth.uid,
+  #                        email: auth.info.email,
+  #                        password: Devise.friendly_token[0,20],
+  #                        agreement: 1
+  #                        )
+  #   end
+  #   user
+  # end
 
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-    end
-  end
+  # def self.find_for_instagram_for_oauth(auth, signed_in_resource=nil)
+  #   user = User.where(:provider => auth.provider, :uid => auth.uid).first
+  #   unless user
+  #     user = User.create(username: auth.info.name,
+  #                        provider: auth.provider,
+  #                        uid: auth.uid,
+  #                        email: User.info.email,
+  #                        password: Devise.friendly_token[0,20],
+  #                        agreement: 1
+  #                        )
+  #   end
+  #   user
+  # end
+
+  # def self.from_omniauth(auth)
+  #   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+  #     user.email = auth.info.email
+  #     user.password = Devise.friendly_token[0,20]
+  #   end
+  # end
 
     private
     # 通常サインアップ時のuid用、Twitter OAuth認証時のemail用にuuidな文字列を生成
