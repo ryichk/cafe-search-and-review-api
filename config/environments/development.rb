e4c27146717cd82084293fe21f4532b2a2e06b38
Ichiki Ryo <tohodaiichiki@gmail.com>
2019-01-20 14:46:36 +0900

    delete webpack

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,5 @@
 Rails.application.configure do
   # Verifies that versions and hashed value of the package contents in the project's package.json
-config.webpacker.check_yarn_integrity = true
 
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -44,7 +43,7 @@ config.webpacker.check_yarn_integrity = true
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-  config.webpacker.check_yarn_integrity = false
+
   # Suppress logger output for asset requests.
   config.assets.quiet = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Verifies that versions and hashed value of the package contents in the project's package.json
-config.webpacker.check_yarn_integrity = false
+
 
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -91,7 +91,7 @@ config.webpacker.check_yarn_integrity = false
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.webpacker.check_yarn_integrity = false
+
 
   # Setup the mailer config
   # Use SendGrid - Add-ons - Heroku
