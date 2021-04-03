90fdc4a9c6228d8fdf5d6a4655e21e3c281e13ea
ryichk <ryichk@gmail.com>
2021-04-03 17:36:06 +0900

    Changed the inheritance source of ApplicationController to ActionController::API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :detect_locate
